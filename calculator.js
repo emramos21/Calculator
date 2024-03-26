f5290022953281143079094c4c5fad1a70b394ad
emramos21 <146121941+emramos21@users.noreply.github.com>
2024-03-26 16:47:05 +0000

    setting up js and fixing the button layout

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let firstNum = first button
+let secNum = 2nd button 
+let addNum = firstNum + secNum
+let subNum = firstNum - secNum
+let multiplyNum = firstNum * secNum
+let dividNum = firstNum / secNum
+
+function add(firstNum, secNum)
+
+
+
+
+const numbers = [];
+const answers = [];
+const operation = [];
