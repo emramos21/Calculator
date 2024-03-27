03e79b9f2d420d127580bc59cdcfb054597ebb66
emramos21 <146121941+emramos21@users.noreply.github.com>
2024-03-27 14:36:50 +0000

    changing html again

--- calculator.js
+++ calculator.js
@@ -1,12 +1,24 @@
-const display = document.getElementById("display");
-function appendToScreen(input){
-    display.value += input;
-}
-function clearScreen(){
-    display.value = "";
-}
-function calculate(){
-
-}
+let operator = '';
+let perviousValue = '';
+let currentValue = '';
 
 
+document.addEventListener("DOMContentLoaded", function(){
+    //this stores all HTML components into JS
+    let clear = document.querySelector("clear");
+    let equal = document.querySelector("equal");
+    let decimal = document.querySelector("dot");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
+    }))
+})
+
+function handleNumber(num){
+    currentValue += num;
+}
